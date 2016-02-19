7265553b584bd7780c1aef1bc34381e0ae2d7a3e
逍遥 <chg.blog@163.com>
2016-02-19 19:06:02 +0800

    修改课程信息

--- Test1/Controllers/Test1Controller.cs
+++ Test1/Controllers/Test1Controller.cs
@@ -5,18 +5,53 @@ using System.Web;
 using System.Web.Mvc;
 using Common;
 using Test1.Models;
+using Common.Models;
+using System.Net;
+using System.Data.Entity;
 
 namespace Test1.Controllers
 {
     public class Test1Controller : Controller
     {
+        private clsConn.ConnDBContext db = new clsConn.ConnDBContext();
+
         //
         // GET: /Test1/
         public ActionResult Index(string name, int id = 0)
         {
-            var a = "aaa";
-            a.PrintString();
-            return View();
+            var arrKeCheng = (from kecheng in db.Exam_e_CourseInfo select kecheng).Take(10);
+            return View(arrKeCheng.ToList());
+        }
+
+        public ActionResult Modify(string KeChengDaiMa)
+        {
+            if (string.IsNullOrEmpty(KeChengDaiMa))
+            {
+                ViewBag.ErrorInfo = "参数错误";
+                return View("Error");
+            }
+            var modKeCheng = db.Exam_e_CourseInfo.Where(KeCheng => KeCheng.CourseCode == KeChengDaiMa).FirstOrDefault();
+            if (modKeCheng == null)
+            {
+                ViewBag.ErrorInfo = "没有找到该课程";
+                return View("Error");
+            }
+            else
+            {
+                return View(modKeCheng);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Modify(Exam_e_CourseInfo modKeCheng)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Entry(modKeCheng).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(modKeCheng);
         }
 
         public ActionResult Submit()
