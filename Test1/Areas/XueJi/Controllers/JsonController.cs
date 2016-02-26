dc1a4188aa2632cd2f9663507d94d50240a50b29
逍遥 <chg.blog@163.com>
2016-02-26 09:54:15 +0800

    添加区域及Ajax提交Json数据给控制器

---
+++ Test1/Areas/XueJi/Controllers/JsonController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Common.Models;
+
+namespace Test1.Areas.XueJi.Controllers
+{
+    public class JsonController : Controller
+    {
+        //
+        // GET: /XueJi/Json/
+        public ActionResult Index()
+        {
+            Student studentModel = new Student();
+            studentModel.XueHao = "001";
+            studentModel.XingMing = "张三";
+            studentModel.NianLing = 25;
+
+            return View(studentModel);
+        }
+
+        public ActionResult SaveStudent(Student studentModel)
+        {
+            //保存数据
+
+            //保存成功后返回保存的数据
+            return Json(studentModel);
+        }
+
+        public ActionResult GetNewStudent()
+        {
+            Student studentModel = new Student();
+            studentModel.XueHao = "002";
+            studentModel.XingMing = "王五";
+            studentModel.NianLing = 26;
+
+            //返回Json格式
+            return Json(studentModel);
+        }
+
+	}
+}
---
+++ Test1/Areas/XueJi/XueJiAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Test1.Areas.XueJi
+{
+    public class XueJiAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "XueJi";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "XueJi_default",
+                "XueJi/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
