abff96d62685d2cbeacccebcb38ff70168d2b578
逍遥 <chg.blog@163.com>
2016-03-10 13:34:32 +0800

    自定义Helper

---
+++ Test1/Common/CustomHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test1.Common
+{
+    public static class CustomHelpers
+    {
+        public static MvcHtmlString StripHtml(this HtmlHelper html, string input)
+        {
+            return new MvcHtmlString(System.Text.RegularExpressions.Regex.Replace(input, "<.*?>", string.Empty));
+        }
+    }
+}
