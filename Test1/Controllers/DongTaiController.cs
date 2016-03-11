4ae2e806bdd14dcf152c3c7ae520d25f097ffb32
逍遥 <chg.blog@163.com>
2016-03-11 13:51:18 +0800

    动态获取全部数据

--- Test1/Controllers/DongTaiController.cs
+++ Test1/Controllers/DongTaiController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Test1.Common;
 using Common.Models;
+using System.Collections;
 
 namespace Test1.Controllers
 {
@@ -18,6 +19,18 @@ namespace Test1.Controllers
             var courses = db.Database.SqlQueryForDynamic("SELECT * FROM Exam_e_CourseInfo");
 
             return View(courses);
+        }
+
+        public ActionResult Modify(string KeChengDaiMa)
+        {
+            string strSQL = "SELECT TOP 1 * FROM Exam_e_CourseInfo WHERE 1=1";
+            if(!string.IsNullOrEmpty(KeChengDaiMa))
+            {
+                strSQL += string.Format(" AND CourseCode='{0}'", KeChengDaiMa);
+            }
+            IEnumerable objCourse = db.Database.SqlQueryForDynamic(strSQL);
+
+            return View(objCourse);
         }
 	}
 }
